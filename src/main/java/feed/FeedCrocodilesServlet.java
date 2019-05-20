52de6f9a28eb6cf3d2902dff291a0cb7f3e8f5c9
ventouristeam <ventouristeam@gmail.com>
2019-05-20 15:16:57 +0300

    Initial commit

---
+++ src/main/java/feed/FeedCrocodilesServlet.java
@@ -0,0 +1,16 @@
+package feed;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FeedCrocodilesServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h3>Feed the crocodiles!</h3>");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Just a dummy jsp. Send the request to a servlet!</h2>
+</body>
+</html>
